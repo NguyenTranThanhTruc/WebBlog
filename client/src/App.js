fa9bf846f68dbf7d26accba79e28bc8475270bde
NguyenTranThanhTruc <thanhtrucnguyentran0810@gmail.com>
2020-11-08 14:51:32 +0700

    handle register with redux

--- client/src/App.js
+++ client/src/App.js
@@ -5,21 +5,27 @@ import Landing from './components/layout/Landing.jsx';
 import Register from './components/auth/Register.jsx';
 import Login from './components/auth/Login.jsx';
 
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Route exact path='/' component={Landing} />
-      <section className="container">
-        <switch>
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/login' component={Login} />
-        </switch>
-      </section>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className="container">
+          <switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 
 );
 
